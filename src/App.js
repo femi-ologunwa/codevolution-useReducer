807da572c0bee718d3c77ce6a3fd3ea0e738c5bc
Femi <ologunwafemi@gmail.com>
2021-05-11 03:03:22 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+   return (
+      <main>
+         <div className='container'>
+            <h2>Hello useReducer</h2>;
+         </div>
+      </main>
+   );
+}
+
+export default App;
